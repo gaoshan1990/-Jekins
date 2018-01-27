ba8758822bc3694cbd7eabef206f1f3f1d0d1da7
gaoshan <gaoshan@gaoshandeMacBook-Pro-2.local>
2018-01-27 17:37:25 +0800

    创建一个项目

---
+++ 测试Jekins/测试Jekins/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  测试Jekins
+//
+//  Created by gaoshan on 2018/1/27.
+//  Copyright © 2018年 gaoshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 测试Jekins/测试Jekins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  测试Jekins
+//
+//  Created by gaoshan on 2018/1/27.
+//  Copyright © 2018年 gaoshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
